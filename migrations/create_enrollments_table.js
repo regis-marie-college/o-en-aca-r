59d595f50ca08833bb61c8c245cd89d2cdefd9dc
tony-tripulca <tony.tripulca@gmail.com>
2026-02-25 11:36:15 +0800

    [feat] Test migration files

--- migrations/create_enrollments_table.js
+++ migrations/create_enrollments_table.js
@@ -1,20 +1,12 @@
 const db = require("../services/supabase");
 
 async function up() {
-  const table = "users";
+  const table = "enrollments";
 
   await db.query(`
     create table if not exists ${table} (
       id uuid primary key default gen_random_uuid(),
-      last_name text not null,
-      first_name text not null,
-      middle_name text,
-      username text,
-      email text not null,
-      mobile text,
-      password text not null,
-      type text,
-      verified_at timestamp,
+      user_id text not null,
       created_at timestamp default now(),
       updated_at timestamp default now(),
       deleted_at timestamp
