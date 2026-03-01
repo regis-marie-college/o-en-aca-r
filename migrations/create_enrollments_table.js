d1ce2a468568ee355151f19568061c672ff2bdfe
ikeladan <adanjymn@gmail.com>
2026-03-01 16:30:01 +0800

    Updates From Master

--- migrations/create_enrollments_table.js
+++ migrations/create_enrollments_table.js
@@ -4,16 +4,26 @@ async function up() {
   const table = "enrollments";
 
   await db.query(`
+    create extension if not exists "pgcrypto";
+
     create table if not exists ${table} (
       id uuid primary key default gen_random_uuid(),
-      user_id text not null,
+
+      last_name varchar(100) not null,
+      first_name varchar(100) not null,
+      middle_name varchar(100),
+
+      email varchar(150) not null,
+      mobile_number varchar(15) not null,
+      birthday date not null,
+
       created_at timestamp default now(),
       updated_at timestamp default now(),
       deleted_at timestamp
     );
   `);
 
-  console.log(`${table} table created`);
+  console.log("${table} table created");
 }
 
 async function down() {
@@ -23,7 +33,7 @@ async function down() {
     drop table if exists ${table};
   `);
 
-  console.log(`${table} table dropped`);
+  console.log("${table} table dropped");
 }
 
 module.exports = { up, down };
