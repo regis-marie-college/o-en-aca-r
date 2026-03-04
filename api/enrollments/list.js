29b0cc4bf341fd7c0ded26fdd793f91b0147d186
John Dariel Adan <adanjymn@gmail.com>
2026-03-04 18:11:53 +0800

    sample edit

--- api/enrollments/list.js
+++ api/enrollments/list.js
@@ -1,6 +1,6 @@
 const { okay, notAllowed, badRequest } = require("../../lib/response");
 const db = require("../../services/supabase");
-
+//edit ko muna to
 module.exports = async (req, res) => {
   if (req.method !== "GET") {
     return notAllowed(res);
